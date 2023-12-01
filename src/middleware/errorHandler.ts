68bc9f8a978feb53d1dbb08e24a79e20c23e1985
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2023-12-02 00:06:11 +0300

    error handler - adding console log in the error handler 500

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { RouteError } from '../Errors/RouteError';
+import { RouteError } from "../Errors/RouteError";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof RouteError) {
-        return res.status(err.statusCode).send(err.message);
-    } else {
-        // in this state the error is un-expected error where there is no status code in the error object
-        res.status(500).send(err.message)
-    }
-}
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RouteError) {
+    return res.status(err.statusCode).send(err.message);
+  }
+  // in this state the error is un-expected error where there is no status code in the error object
+  console.error(err);
+  res.status(500).send(err.message);
+};
